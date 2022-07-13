a1532b59d9f64ece1c44aacda41d29b74f67c4a7
Kaue Gonçalves <kauevini10@outlook.com>
2022-07-13 18:11:14 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import logo from '../../assets/img/logo.svg'
+import './style.css'
+function Header() {
+    return (
+         <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/kaue-gon%C3%A7alves-2630271b0/"> Kauê Gonçalves</a>
+                </p>
+            </div>
+            
+            </header>
+  
+  
+  
+   
+  
+    )
+  }
+  
+  export default Header
+  
