a749263f625834b7a60583744d46e8f5527ef4d6
Kaue Gonçalves <kauevini10@outlook.com>
2022-07-16 20:58:27 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,8 +1,20 @@
+import axios from 'axios';
 import icon from'../../assets/img/Vector.svg';
+import { BASE_URL } from '../../utills/request';
 import '../style.css';
-function NotificationButton() {
+
+type props = {
+    saleId: number;
+}
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+          console.log("SUCESSO")
+    })
+}
+function NotificationButton({saleId} : props) {
     return(
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
         <img src={icon} alt="Notificar" />    
          </div>
     )
